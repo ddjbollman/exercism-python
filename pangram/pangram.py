3363918d017664c775aa8343405dc20b448d13c7
joseph bollman <joseph.bollman@gmail.com>
2017-11-07 17:41:05 -0800

    completed pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,26 @@
 def is_pangram(sentence):
-    pass
+    sentence_list = []
+    for element in sentence:
+        sentence_list += element.lower()
+    for bit in alphabit_list():
+        if bit not in sentence_list:
+            return False
+    return True
+
+
+
+
+
+
+def alphabit_list():
+    alphabit = 'abcdefghijklmnopqrstuvwxyz'
+    alphabit = list(alphabit)
+    return alphabit
+
+
+
+# every letter atleas once
+# repeating letters is okay.    
+# to lower
+#underscores ok
+# spaes okay
